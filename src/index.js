37919799f602bad2684d9fe12f6c2fbf6ea9d90e
dianajyoo <diana.yooj@gmail.com>
2018-11-13 10:16:42 -0500

    added some css and input field

---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+const INPUT_URL = "http://localhost:3000/text_inputs"
+document.addEventListener('DOMContentLoaded', () => {
+
+  // fetch(INPUT_URL)
+  // .then(res => res.json() )
+  // .then(console.log)
+
+})
