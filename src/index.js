1aaa8de4f2465f652fdd464151761cce5c5b3001
dianajyoo <diana.yooj@gmail.com>
2018-11-14 09:40:59 -0500

    bulma button and input field

--- src/index.js
+++ src/index.js
@@ -1,9 +1,33 @@
-
 const INPUT_URL = "http://localhost:3000/text_inputs"
+let text
+
 document.addEventListener('DOMContentLoaded', () => {
 
-  // fetch(INPUT_URL)
-  // .then(res => res.json() )
-  // .then(console.log)
+  document.addEventListener('submit', (event) => {
+    event.preventDefault()
+    text = event.target[0].value
+
+    // debugger
+    fetch(INPUT_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        text: text
+      })
+    })
+    .then(res => res.json())
+    .then( json => {
+
+      // for (let i = 0; i < json['document_tone']['tones'].length; i++) {
+      //   console.log(json['document_tone']['tones'][i])
+      // }
+      fetch(INPUT_URL)
+      .then(res => res.json())
+      .then(console.log)
+    })
+  })
 
 })
